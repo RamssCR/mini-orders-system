648633b3f003fbb04d8f88d2369eaec0352500d6
RamssCR <ramsesgpm@gmail.com>
2026-05-21 15:43:09 -0500

    test: added an entry point to test configuration

---
+++ app-gateway/jest.config.ts
@@ -0,0 +1,27 @@
+import type { Config } from 'jest';
+import { pathsToModuleNameMapper } from 'ts-jest';
+import fs from 'fs';
+import path from 'path';
+
+const tsconfigPath = path.resolve('app-gateway', 'tsconfig.json');
+const tsconfigContent = fs.readFileSync(tsconfigPath, 'utf8');
+const { compilerOptions } = JSON.parse(tsconfigContent);
+
+const config: Config = {
+  displayName: 'app-gateway',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverageFrom: ['**/*.(t|j)s'],
+  coverageDirectory: '../coverage/app-gateway',
+  testEnvironment: 'node',
+
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions?.paths || {}, {
+    prefix: '<rootDir>/',
+  }),
+};
+
+export default config;
---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  projects: [
+    '<rootDir>/app-gateway/jest.config.ts',
+    '<rootDir>/audit-service/jest.config.ts',
+    '<rootDir>/orders-service/jest.config.ts',
+  ],
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+};
+
+export default config;
