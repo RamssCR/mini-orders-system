cb19acb166ade4fa34ad409cf6951639cf9f56e7
RamssCR <ramsesgpm@gmail.com>
2026-05-21 16:38:10 -0500

    feat: added order and database module

--- orders-service/src/app.module.ts
+++ orders-service/src/app.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from '#common/modules/database.module';
+import { Module } from '@nestjs/common';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
