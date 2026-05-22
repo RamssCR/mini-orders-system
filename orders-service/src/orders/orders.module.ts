1cd3d69f2c73b84488ae6e1a7550b708a5ad4a74
RamssCR <ramsesgpm@gmail.com>
2026-05-21 23:11:36 -0500

    feat: included order entity in typeorm configuration module

--- orders-service/src/orders/orders.module.ts
+++ orders-service/src/orders/orders.module.ts
@@ -1,9 +1,14 @@
+import { DatabaseTransactionService } from '#common/providers/query-runner.service';
 import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
+import { Order } from './entities/order.entity';
 import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Order])],
   controllers: [OrdersController],
-  providers: [OrdersService],
+  providers: [DatabaseTransactionService, OrdersService],
+  exports: [TypeOrmModule],
 })
 export class OrdersModule {}
