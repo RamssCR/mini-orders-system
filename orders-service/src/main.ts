9a08e3f50f36076609dbb0c0c5ee8e058b5c3038
RamssCR <ramsesgpm@gmail.com>
2026-05-21 23:05:09 -0500

    feat: setup rabbitmq in the orders's bootstrap function

--- orders-service/src/main.ts
+++ orders-service/src/main.ts
@@ -1,8 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { RABBITMQ_QUEUE, RABBITMQ_URLS } from '#config/environment';
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: RABBITMQ_URLS,
+        queue: RABBITMQ_QUEUE,
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
+  await app.listen();
 }
-bootstrap();
+
+void bootstrap();
