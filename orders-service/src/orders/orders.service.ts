8fa287493d82e00c145a5e9f9c710c9e3d1f86e8
RamssCR <ramsesgpm@gmail.com>
2026-05-21 23:13:13 -0500

    feat: created all the microservice's services

--- orders-service/src/orders/orders.service.ts
+++ orders-service/src/orders/orders.service.ts
@@ -1,4 +1,141 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { In, Repository } from 'typeorm';
+import { CreateOrderDto } from './dtos/create-order.dto';
+import { DatabaseTransactionService } from '#common/providers/query-runner.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-items.entity';
+import { OrdersQueryDto } from './dtos/orders-query.dto';
+import { Paginated } from '#common/types/pagination';
+import { Product } from './entities/product.entity';
+import { QueryRunner } from 'typeorm/browser';
+import { User } from './entities/user.entity';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+    private readonly transactionService: DatabaseTransactionService,
+  ) {}
+
+  async findPaginated(queries: OrdersQueryDto): Promise<Paginated<Order>> {
+    const { limit, offset } = queries;
+
+    const [orders, total] = await this.orderRepository.findAndCount({
+      skip: offset,
+      take: limit,
+      where: { status: queries?.status, user: { id: queries?.userId } },
+    });
+
+    return {
+      data: orders,
+      total,
+      pages: Math.ceil(total / limit),
+    };
+  }
+
+  create(payload: CreateOrderDto): Promise<Order> {
+    const { products: productsDto } = payload;
+
+    return this.transactionService.execute<Order>(async (queryRunner) => {
+      const user = await this.findOrCreateUser(payload, queryRunner);
+
+      const products = await this.getProducts(productsDto, queryRunner);
+      const itemsMap = new Map(
+        productsDto.map((product) => [product.id, product.quantity]),
+      );
+
+      const order = queryRunner.manager.create(Order, {
+        user,
+        quantity: 0,
+      });
+      await queryRunner.manager.save(order);
+
+      const totalQuantity = await this.processOrderItems(
+        products,
+        itemsMap,
+        order,
+        queryRunner,
+      );
+
+      order.quantity = totalQuantity;
+      return await queryRunner.manager.save(order);
+    });
+  }
+
+  async updateStatus(id: string): Promise<void> {
+    const order = await this.orderRepository.findOneOrFail({ where: { id } });
+    if (order.status === 'completed') return;
+
+    order.status = 'completed';
+    await this.orderRepository.save(order);
+  }
+
+  private async findOrCreateUser(
+    { documentId, name, phone }: Omit<CreateOrderDto, 'products'>,
+    queryRunner: QueryRunner,
+  ): Promise<User> {
+    let user = await queryRunner.manager.findOne(User, {
+      where: { documentId },
+      lock: { mode: 'pessimistic_write' },
+    });
+
+    if (!user) {
+      user = queryRunner.manager.create(User, { documentId, name, phone });
+      await queryRunner.manager.save(user);
+    }
+
+    return user;
+  }
+
+  private async getProducts(
+    productsDto: CreateOrderDto['products'],
+    queryRunner: QueryRunner,
+  ): Promise<Product[]> {
+    const ids = productsDto.map((product) => product.id);
+    const products = await queryRunner.manager.find(Product, {
+      where: { id: In(ids) },
+      lock: { mode: 'pessimistic_write' },
+    });
+
+    if (products.length !== productsDto.length)
+      throw new BadRequestException(`Some products were not found`);
+
+    return products;
+  }
+
+  private async processOrderItems(
+    products: Product[],
+    itemsMap: Map<string, number>,
+    order: Order,
+    queryRunner: QueryRunner,
+  ): Promise<number> {
+    let totalQuantity = 0;
+    const orderItems: OrderItem[] = [];
+
+    for (const product of products) {
+      const quantity = itemsMap.get(product.id) ?? 0;
+
+      if (product.stock < quantity)
+        throw new BadRequestException(
+          `Product ${product.name} is out of stock`,
+        );
+
+      product.stock -= quantity;
+      await queryRunner.manager.save(product);
+
+      const orderItem = queryRunner.manager.create(OrderItem, {
+        quantity,
+        order,
+        product,
+      });
+
+      orderItems.push(orderItem);
+      totalQuantity += quantity;
+    }
+
+    await queryRunner.manager.save(OrderItem, orderItems);
+    return totalQuantity;
+  }
+}
