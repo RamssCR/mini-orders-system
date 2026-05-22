74660bf8493e3da0b35918004ae704ecbd8216e6
RamssCR <ramsesgpm@gmail.com>
2026-05-21 23:10:45 -0500

    feat: created the main 3 message pattern channels for the orders microservice

--- orders-service/src/orders/orders.controller.ts
+++ orders-service/src/orders/orders.controller.ts
@@ -1,7 +1,25 @@
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Controller } from '@nestjs/common';
+import { CreateOrderDto } from './dtos/create-order.dto';
+import { OrdersQueryDto } from './dtos/orders-query.dto';
 import { OrdersService } from './orders.service';
 
 @Controller()
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
+
+  @MessagePattern('orders')
+  findAll(@Payload() queries: OrdersQueryDto) {
+    return this.ordersService.findPaginated(queries);
+  }
+
+  @MessagePattern('place.order')
+  create(@Payload() payload: CreateOrderDto) {
+    return this.ordersService.create(payload);
+  }
+
+  @MessagePattern('order.status')
+  changeStatus(@Payload('id') id: string) {
+    return this.ordersService.updateStatus(id);
+  }
 }
