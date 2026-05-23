6dd4569ebc97d7864d8f130ff92b91e35ffa81df
RamssCR <ramsesgpm@gmail.com>
2026-05-23 17:25:02 -0500

    feat: created orders controller

--- app-gateway/src/orders/orders.controller.ts
+++ app-gateway/src/orders/orders.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
-import { OrdersService } from './orders.service';
+import {
+  Body,
+  Controller,
+  Get,
+  Inject,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
+import { CreateOrderDto } from './dtos/create-order.dto';
+import { OrdersQueryDto } from './dtos/orders-query.dto';
+import { RABBITMQ_NAME } from '#config/environment';
 
-@Controller()
+@Controller('orders')
 export class OrdersController {
-  constructor(private readonly ordersService: OrdersService) {}
+  constructor(
+    @Inject(RABBITMQ_NAME)
+    private readonly client: ClientProxy,
+  ) {}
+
+  @Get()
+  findAll(@Query() queries: OrdersQueryDto) {
+    return this.client.send('orders', queries);
+  }
+
+  @Post()
+  create(@Body() payload: CreateOrderDto) {
+    return this.client.send('place.order', payload);
+  }
+
+  @Put()
+  changeStatus(@Param('orderId') orderId: string) {
+    return this.client.emit('order.status', { orderId });
+  }
 }
