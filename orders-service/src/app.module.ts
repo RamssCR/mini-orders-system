a3d2e56a48a0b1dca20ce23d711f804374404aa4
RamssCR <ramsesgpm@gmail.com>
2026-05-23 15:29:46 -0500

    feat: added TCP client connection and pipe validation

--- orders-service/src/app.module.ts
+++ orders-service/src/app.module.ts
@@ -1,11 +1,28 @@
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { TCP_HOST, TCP_NAME, TCP_PORT } from '#config/environment';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from '#common/modules/database.module';
 import { Module } from '@nestjs/common';
 import { OrdersModule } from './orders/orders.module';
+import { PipesModule } from '#common/pipes/pipes.module';
 
 @Module({
-  imports: [OrdersModule, DatabaseModule],
+  imports: [
+    ClientsModule.register([
+      {
+        name: TCP_NAME,
+        transport: Transport.TCP,
+        options: {
+          host: TCP_HOST,
+          port: TCP_PORT,
+        },
+      },
+    ]),
+    OrdersModule,
+    DatabaseModule,
+    PipesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
