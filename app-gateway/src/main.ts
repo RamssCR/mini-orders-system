da91be97e07b2bd1c436b53d427e51db4fa775d8
RamssCR <ramsesgpm@gmail.com>
2026-05-23 16:30:19 -0500

    feat: setup gateway bootstrap configuration

--- app-gateway/src/main.ts
+++ app-gateway/src/main.ts
@@ -1,13 +1,23 @@
+import { ALLOWED_ORIGIN, NODE_ENV, PORT } from '#config/environment';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { NestFactory } from '@nestjs/core';
-import { PORT } from '#config/environment';
 import helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const logger = new Logger('AppGateway');
 
+  app.setGlobalPrefix('/api/v1');
+  app.set('query parser', 'extended');
   app.use(helmet());
+  app.enableCors({
+    origin: NODE_ENV === 'development' ? '*' : ALLOWED_ORIGIN,
+  });
+
   await app.listen(PORT);
+  logger.log(`App running at ${await app.getUrl()}`);
 }
 
 void bootstrap();
