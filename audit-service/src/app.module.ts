8e5d156a7a618a2a900b9438308dac7c710e9820
RamssCR <ramsesgpm@gmail.com>
2026-05-23 15:28:56 -0500

    feat: added audits module and pipe validation

--- audit-service/src/app.module.ts
+++ audit-service/src/app.module.ts
@@ -1,10 +1,12 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuditsModule } from './audits/audits.module';
 import { DatabaseModule } from '#common/modules/database.module';
 import { Module } from '@nestjs/common';
+import { PipesModule } from '#common/pipes/pipes.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [AuditsModule, DatabaseModule, PipesModule],
   controllers: [AppController],
   providers: [AppService],
 })
