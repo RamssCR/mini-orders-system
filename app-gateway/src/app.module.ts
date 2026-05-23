741ae04bcceee605ba69aa43abb5252f25fc52fb
RamssCR <ramsesgpm@gmail.com>
2026-05-23 16:28:22 -0500

    feat: added microservices and brokers modules

--- app-gateway/src/app.module.ts
+++ app-gateway/src/app.module.ts
@@ -1,11 +1,20 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuditsModule } from './audits/audits.module';
+import { BrokersModule } from '#common/modules/brokers.module';
 import { Module } from '@nestjs/common';
+import { OrdersModule } from './orders/orders.module';
 import { PipesModule } from '#common/pipes/pipes.module';
 import { ThrottlerModule } from '#common/modules/throttler.module';
 
 @Module({
-  imports: [PipesModule, ThrottlerModule],
+  imports: [
+    AuditsModule,
+    BrokersModule,
+    OrdersModule,
+    PipesModule,
+    ThrottlerModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
