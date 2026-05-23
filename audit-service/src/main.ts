0a711530bf66b5fceed3f86b9162339202b2349b
RamssCR <ramsesgpm@gmail.com>
2026-05-23 15:32:41 -0500

    feat: setup TCP server connection

--- audit-service/src/main.ts
+++ audit-service/src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { TCP_HOST, TCP_PORT } from '#config/environment';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: TCP_HOST,
+        port: TCP_PORT,
+      },
+    },
+  );
+  const logger = new Logger('AuditMicroservice');
+
+  await app.listen();
+  logger.log(
+    `Audit Microservice is listening via TCP on ${TCP_HOST}:${TCP_PORT}`,
+  );
 }
-bootstrap();
+
+void bootstrap();
