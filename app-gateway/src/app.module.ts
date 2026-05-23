82c2de3bc2044bde01f806af15a4e2da82c77e26
RamssCR <ramsesgpm@gmail.com>
2026-05-23 15:28:06 -0500

    feat: added pipe validation

--- app-gateway/src/app.module.ts
+++ app-gateway/src/app.module.ts
@@ -1,10 +1,11 @@
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Module } from '@nestjs/common';
+import { PipesModule } from '#common/pipes/pipes.module';
 import { ThrottlerModule } from '#common/modules/throttler.module';
 
 @Module({
-  imports: [ThrottlerModule],
+  imports: [PipesModule, ThrottlerModule],
   controllers: [AppController],
   providers: [AppService],
 })
