ed45fa67bcfcabe5fa562eb710dbbdbbfbe96e23
RamssCR <ramsesgpm@gmail.com>
2026-05-24 17:26:05 -0500

    fix: moved client registration to their respective module

--- orders-service/src/orders/orders.module.ts
+++ orders-service/src/orders/orders.module.ts
@@ -1,3 +1,9 @@
+import {
+  AUDIT_SERVICE_HOST,
+  AUDIT_SERVICE_PORT,
+  AUDIT_SERVICE_PROXY,
+} from '#config/environment';
+import { ClientsModule, Transport } from '@nestjs/microservices';
 import { DatabaseTransactionService } from '#common/providers/query-runner.service';
 import { Module } from '@nestjs/common';
 import { Order } from './entities/order.entity';
@@ -6,7 +12,19 @@ import { OrdersService } from './orders.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Order])],
+  imports: [
+    ClientsModule.register([
+      {
+        name: AUDIT_SERVICE_PROXY,
+        transport: Transport.TCP,
+        options: {
+          host: AUDIT_SERVICE_HOST,
+          port: AUDIT_SERVICE_PORT,
+        },
+      },
+    ]),
+    TypeOrmModule.forFeature([Order]),
+  ],
   controllers: [OrdersController],
   providers: [DatabaseTransactionService, OrdersService],
   exports: [TypeOrmModule],
