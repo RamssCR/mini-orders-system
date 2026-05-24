4cf9cba89d8d9bdd05790cc9655aead67c8ddcd3
RamssCR <ramsesgpm@gmail.com>
2026-05-24 17:25:25 -0500

    feat: added validation for RPC exceptions

--- audit-service/src/common/pipes/pipes.module.ts
+++ audit-service/src/common/pipes/pipes.module.ts
@@ -1,11 +1,23 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
+import { RpcException } from '@nestjs/microservices';
 
 @Module({
   providers: [
     {
       provide: APP_PIPE,
-      useFactory: () => new ValidationPipe({ transform: true }),
+      useFactory: () =>
+        new ValidationPipe({
+          transform: true,
+          exceptionFactory: (errors) => {
+            const formatted = `${errors[0].property} - ${Object.values(errors[0].constraints ?? {}).join(', ')}`;
+            return new RpcException({
+              message: formatted,
+              status: 400,
+              isRpc: true,
+            });
+          },
+        }),
     },
   ],
 })
